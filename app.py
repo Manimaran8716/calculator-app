119d571ecba2e2dc1fd024df089c453f311ab850
mani <manimaran8716@gmail.com>
2026-05-03 09:21:08 +0000

    my first calculator app

---
+++ app.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Simple Calculator")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Add:", add(a, b))
+print("Sub:", sub(a, b))
+print("Mul:", mul(a, b))
+print("Div:", div(a, b))
