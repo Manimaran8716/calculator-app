1b012962eeda6c8229f22c16a6e467c779a699b3
mani <manimaran8716@gmail.com>
2026-05-03 11:19:29 +0000

    my second branch f2

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return a - b
+if __name__ == "__main__":
+    print("Calculator App Running...")
+    print("2 + 3 =", add(2, 3))
 
-def mul(a, b):
-    return a * b
-
-def div(a, b):
-    return a / b
-
-print("Simple Calculator")
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-print("Add:", add(a, b))
-print("Sub:", sub(a, b))
-print("Mul:", mul(a, b))
-print("Div:", div(a, b))
